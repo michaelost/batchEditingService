3c0a26d48254e2436abbcd74a7e4b6ef1c4bb8c0
michaelost <mykhailo.ostroviy@keenethics.com>
2018-03-24 11:19:40 +0200

    batch

--- app/routes/batch.js
+++ app/routes/batch.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import app from '../app.js';
+import axios from 'axios';
 
 const batchRouter = express.Router();
 
@@ -11,17 +12,49 @@ batchRouter.post('/batch', async function(req, res) {
     } = req.body;
 
     const {
-      url, 
-      httpType,
+      url,
+      verb,
     } = endpoint; 
     
     const {
-      parameters,        // [] of {}
+      parameters,        // [] of { userId: String , age: Number }
     } = payload;
+
+    const requests = [];
+
+    parameters.forEach((parameter) => {
+      const {
+        userId, 
+        age,
+      } = parameter;
+      let currentRequest = null;
+      const URL = `${url}/userId`;
+      switch (verb) {
+        case "POST":
+          currentRequest = axios.post(URL, { age }); 
+          break;
+        case "PATCH":
+          currentRequest = axios.patch(URL, { age });
+          break;
+      }   
+      requests.push(r);
+    });
+
+    const responses = await axios.all(requests);
+
+    const statuses =  responses.map((response, index) => {
+      const {
+        status,
+      } = response;
+      return {
+        status,
+        number: index,
+      }
+    });
 
     return res.json({
       success: true,
-      message: 'batching',
+      responses: statuses,
     });
 
   } catch (err) {
